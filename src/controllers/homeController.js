c131554b11946b46e4c57e8fda6d39d7b386e750
Jessica Andrade <jess.andradec@gmail.com>
2023-02-18 14:01:57 +0100

    Revert "Back-end done :)"
    This reverts commit 2bb833d01268f8212ee868989391c45c7afe5698.

--- src/controllers/homeController.js
+++
@@ -1,7 +0,0 @@
-const Contact = require('../models/ContactModel');
-const contact = require('../models/ContactModel')
-
-exports.index = async (req, res) => {
-  const contacts = await Contact.searchContacts();
-  res.render('index', {contacts: contacts});
-};
